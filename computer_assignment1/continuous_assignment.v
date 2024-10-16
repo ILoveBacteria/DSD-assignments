452944cab9d795e4f7709c219fb3059b8d75c99f
Moein Arabi <moein.mo81@gmail.com>
2024-10-16 14:00:40 +0330

    Add adder module

--- computer_assignment1/continuous_assignment.v
+++ computer_assignment1/continuous_assignment.v
@@ -3,11 +3,22 @@
 module multiplexer #(
     parameter N_BITS = 1
 ) (
-    input [N_BITS:0] in0,
-    input [N_BITS:0] in1,
+    input [N_BITS-1:0] in0,
+    input [N_BITS-1:0] in1,
     input select,
-    output [N_BITS:0] out
+    output [N_BITS-1:0] out
 );
     assign out = (select == 0) ? in0 : 1'bZ;
     assign out = (select == 1) ? in1 : 1'bZ;
+endmodule
+
+module adder #(
+    parameter N_BITS = 1
+) (
+    input [N_BITS-1:0] in0,
+    input [N_BITS-1:0] in1,
+    output [N_BITS-1:0] out,
+    output carry_out
+);
+    assign {carry_out, out} = in0 + in1;    
 endmodule
