3586cdb7d98ca507b455f359c37e12269226f386
Moein Arabi <moein.mo81@gmail.com>
2024-10-18 10:58:41 +0330

    Add adder gate level module

--- computer_assignment1/gate_level.v
+++ computer_assignment1/gate_level.v
@@ -1,9 +1,7 @@
 // gate level
 
 module multiplexer (
-    input in0,
-    input in1,
-    input select,
+    input in0, in1, select,
     output out
 );
     wire in0_and_out, in1_and_out, not_select;
@@ -11,4 +9,16 @@ module multiplexer (
     not(not_select, select);
     and(in1_and_out, in1, not_select);
     or(out, in0_and_out, in1_and_out);
+endmodule
+
+module adder (
+    input a, b, carry_in,
+    output out, carry_out
+);
+    wire and1_to_or, and2_to_or, and3_to_or;
+    xor(out, a, b, carry_in); 
+    and(and1_to_or, a, b);
+    and(and2_to_or, a, carry_in);
+    and(and3_to_or, b, carry_in);
+    or(carry_out, and1_to_or, and2_to_or, and3_to_or); 
 endmodule
