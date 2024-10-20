c1d0be71cf4a855928ab06fab4aefda90f471f6f
Moein Arabi <moein.mo81@gmail.com>
2024-10-20 13:38:11 +0330

    Fix not operator in continuous

--- computer_assignment1/continuous_assignment.v
+++ computer_assignment1/continuous_assignment.v
@@ -33,6 +33,6 @@ module top_level #(
     output carry_out
 );
     wire [N_BITS-1:0] mux_out_adder_in;
-    multiplexer #(N_BITS) mux_instance (in1, !in1+1, select, mux_out_adder_in); // select=0: add, select=1: subtract
+    multiplexer #(N_BITS) mux_instance (in1, ~in1+1, select, mux_out_adder_in); // select=0: add, select=1: subtract
     adder #(N_BITS) adder_instance (mux_out_adder_in, in0, out, carry_out);
 endmodule
