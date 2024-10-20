8d67c10deb302f654327d03a2c89a1bcaccd932d
Romina <romina.shekary@gmail.com>
2024-10-20 10:25:45 +0330

    Fix bugs

--- computer_assignment1/procedural.v
+++ computer_assignment1/procedural.v
@@ -8,9 +8,12 @@ module multiplexer #(
     input select,
     output reg [N_BITS-1:0] out
 );
-    always @(a, b) begin
-        out = (select == 0) ? a : 1'bZ;
-        out = (select == 1) ? b : 1'bZ;
+    always @(a, b, select) begin
+        if (select == 0) begin
+            out = a;
+        end else begin
+            out = b;
+        end
     end
 endmodule
 
@@ -38,6 +41,8 @@ module top_level #(
     output carry_out
 );
     wire [N_BITS-1:0] mux_out_adder_in;
-    multiplexer #(N_BITS) mux_instance (in1, !in1+1, select, mux_out_adder_in); // select=0: add, select=1: subtract
+    wire [N_BITS-1:0] compliment_b;
+    assign compliment_b = ~in1+1;
+    multiplexer #(N_BITS) mux_instance (in1, compliment_b, select, mux_out_adder_in); // select=0: add, select=1: subtract
     adder #(N_BITS) adder_instance (mux_out_adder_in, in0, out, carry_out);
 endmodule
