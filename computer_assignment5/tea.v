2a758f4bec4015bccb8c82ab2ce5b5b4300e5d78
Moein Arabi <moein.mo81@gmail.com>
2024-12-16 17:48:35 +0330

    Change blocking to nonblocking

--- computer_assignment5/tea.v
+++ computer_assignment5/tea.v
@@ -1,6 +1,6 @@
 module tea_pipelined #(
     parameter DELTA = 32'h9E3779B9,
-    parameter ROUNDS = 32
+    parameter ROUNDS = 1
 )(
     input wire clk,
     input wire rst_n,
@@ -18,15 +18,15 @@ module tea_pipelined #(
     // input registers
     always @(posedge clk or negedge rst_n) begin
         if (rst_n == 0) begin
-            v0_pipe[0] = 32'b0;
-            v1_pipe[0] = 32'b0;
-            sum_pipe[0] = 32'b0;
-            key_pipe[0] = 128'b0;
+            v0_pipe[0] <= 32'b0;
+            v1_pipe[0] <= 32'b0;
+            sum_pipe[0] <= 32'b0;
+            key_pipe[0] <= 128'b0;
         end else begin
-            v0_pipe[0] = plaintext[63:32]; // v0 on the left (higher bits)
-            v1_pipe[0] = plaintext[31:0]; // v1 on the right (lower bits)
-            sum_pipe[0] = DELTA;
-            key_pipe[0] = key;
+            v0_pipe[0] <= plaintext[63:32]; // v0 on the left (higher bits)
+            v1_pipe[0] <= plaintext[31:0]; // v1 on the right (lower bits)
+            sum_pipe[0] <= DELTA;
+            key_pipe[0] <= key;
         end
     end
 
@@ -51,44 +51,44 @@ module tea_pipelined #(
             reg [31:0] next_pipe_sum;
 
             always @(*) begin
-                v1_shift_l4 = v1_pipe[i] << 4;
-                v1_shift_r5 = v1_pipe[i] >> 5;
-                v0_shift_l4 = next_pipe_v0 << 4;
-                v0_shift_r5 = next_pipe_v0 >> 5;
+                v1_shift_l4 <= v1_pipe[i] << 4;
+                v1_shift_r5 <= v1_pipe[i] >> 5;
+                v0_shift_l4 <= next_pipe_v0 << 4;
+                v0_shift_r5 <= next_pipe_v0 >> 5;
             end
 
             always @(*) begin
-                next_pipe_v0 = v0_pipe[i] + 
+                next_pipe_v0 <= v0_pipe[i] + 
                                 ((v1_shift_l4 + k0) ^ 
                                     (v1_pipe[i] + sum_pipe[i]) ^ 
                                     (v1_shift_r5 + k1));
                 
-                next_pipe_v1 = v1_pipe[i] + 
+                next_pipe_v1 <= v1_pipe[i] + 
                                 ((v0_shift_l4 + k2) ^ 
                                     (next_pipe_v0 + sum_pipe[i]) ^ 
                                     (v0_shift_r5 + k3));
                 
-                next_pipe_sum = sum_pipe[i] + DELTA;
+                next_pipe_sum <= sum_pipe[i] + DELTA;
             end
 
             // Pipeline registers for each stage
             always @(posedge clk or negedge rst_n) begin
                 if (!rst_n) begin
-                    v0_pipe[i+1] = 32'b0;
-                    v1_pipe[i+1] = 32'b0;
-                    sum_pipe[i+1] = 32'b0;
-                    key_pipe[i+1] = 128'b0;
+                    v0_pipe[i+1] <= 32'b0;
+                    v1_pipe[i+1] <= 32'b0;
+                    sum_pipe[i+1] <= 32'b0;
+                    key_pipe[i+1] <= 128'b0;
                 end else begin
                     // v0 update
-                    v0_pipe[i+1] = next_pipe_v0;
+                    v0_pipe[i+1] <= next_pipe_v0;
                     
                     // v1 update
-                    v1_pipe[i+1] = next_pipe_v1;
+                    v1_pipe[i+1] <= next_pipe_v1;
                     
                     // Sum update for next stage
-                    sum_pipe[i+1] = next_pipe_sum;
+                    sum_pipe[i+1] <= next_pipe_sum;
 
-                    key_pipe[i+1] = {k0, k1, k2, k3};
+                    key_pipe[i+1] <= {k0, k1, k2, k3};
                 end
             end
         end
