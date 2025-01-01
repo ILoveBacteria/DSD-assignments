3ad253b1a5e10da8c3c55f25c29c83251fe1e41f
Moein Arabi <moein.mo81@gmail.com>
2025-01-01 16:57:15 +0330

    Update main.c

--- computer_assignment7/AXI4LiteSlaveReg/AxiTest01/AxiTest01.sdk/Test01/src/main.c
+++ computer_assignment7/AXI4LiteSlaveReg/AxiTest01/AxiTest01.sdk/Test01/src/main.c
@@ -10,16 +10,29 @@ int main()
 	for(;;)
 	{
 		xil_printf("--------------------------------\n\r");
-		for(i = 0; i < 16; i++)
+		// a = 5
+		mem[AXI_ADDR0 + 0] = 5;
+		for(i = 1; i < 8; i++)
 		{
-			mem[AXI_ADDR0+i] = 1000+i;
-			xil_printf("M[%d] = %d\n\r", i, 1000+i);
+			mem[AXI_ADDR0+i] = 0;
+			xil_printf("M[%d] = %d\n\r", i, 0);
 		}
-		sleep(1);
+		// b = 3
+		mem[AXI_ADDR0 + 8] = 3;
+		for(i = 9; i < 16; i++)
+		{
+			mem[AXI_ADDR0+i] = 0;
+			xil_printf("M[%d] = %d\n\r", i, 0);
+		}
+
+		// read
+		sleep(5);
+		xil_printf("--------------------------------\n\r");
 		for(i = 0; i <= 16; i++)
 		{
 			out = mem[AXI_ADDR0+i];
-			xil_printf("Read %d\n\r", out);
+			xil_printf("Read %d = %d\n\r", i, out);
+			sleep(1);
 		}
 		sleep(5);
 	}
