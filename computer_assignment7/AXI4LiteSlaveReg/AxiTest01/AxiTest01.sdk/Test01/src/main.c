2eccd963296128c60291775f891a27b1550d7553
Moein Arabi <moein.mo81@gmail.com>
2025-01-02 15:57:01 +0330

    Update main.c

--- computer_assignment7/AXI4LiteSlaveReg/AxiTest01/AxiTest01.sdk/Test01/src/main.c
+++ computer_assignment7/AXI4LiteSlaveReg/AxiTest01/AxiTest01.sdk/Test01/src/main.c
@@ -1,39 +1,63 @@
 #define AXI_ADDR0 0x0400000000
 volatile unsigned int *AXIAddr0 = (volatile unsigned int *) AXI_ADDR0;
 
+unsigned int *mem = AXI_ADDR0;
+
+void read()
+{
+	unsigned int out;
+	xil_printf("-------------READ--------------\n\r");
+	for(int i = 0; i <= 22; i++)
+	{
+		out = mem[AXI_ADDR0+i];
+		xil_printf("Read %d = %d\n\r", i, out);
+		sleep(1);
+	}
+	sleep(5);
+}
+
+void write_number(int a[8], int b[8])
+{
+	int i = 7;
+	int j = 0;
+	for(; i >= 0; i--)
+	{
+		mem[AXI_ADDR0 + j] = a[i];
+		mem[AXI_ADDR0 + j + 8] = b[i];
+		j++;
+	}
+}
+
+void write()
+{
+	xil_printf("-------------WRITE-------------\n\r");
+	// a = 5
+	mem[AXI_ADDR0 + 0] = 5;
+	for(int i = 1; i < 8; i++)
+	{
+		mem[AXI_ADDR0+i] = 0;
+	}
+
+	// b = 3
+	mem[AXI_ADDR0 + 8] = 3;
+	for(int i = 9; i < 16; i++)
+	{
+		mem[AXI_ADDR0+i] = 0;
+	}
+}
+
+
 int main()
 {
-	int i;
-	unsigned int *mem = AXI_ADDR0;
-	unsigned int out;
+//	int a[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 5};
+//	int b[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 3};
+
+	int a[8] = {0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111, 0x11111111};
+	int b[8] = {0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0};
 
 	for(;;)
 	{
-		xil_printf("--------------------------------\n\r");
-		// a = 5
-		mem[AXI_ADDR0 + 0] = 5;
-		for(i = 1; i < 8; i++)
-		{
-			mem[AXI_ADDR0+i] = 0;
-			xil_printf("M[%d] = %d\n\r", i, 0);
-		}
-		// b = 3
-		mem[AXI_ADDR0 + 8] = 3;
-		for(i = 9; i < 16; i++)
-		{
-			mem[AXI_ADDR0+i] = 0;
-			xil_printf("M[%d] = %d\n\r", i, 0);
-		}
-
-		// read
-		sleep(5);
-		xil_printf("--------------------------------\n\r");
-		for(i = 0; i <= 16; i++)
-		{
-			out = mem[AXI_ADDR0+i];
-			xil_printf("Read %d = %d\n\r", i, out);
-			sleep(1);
-		}
-		sleep(5);
+		write_number(a, b);
+		read();
 	}
 }
